95650d800e956949a930680e462771cc76501c8c
Shom Bandopadhaya <shom@bandopadhaya.com>
2022-05-04 01:05:36 -0500

    Added features to set header and tag, improved documentation.

--- ntfy.el
+++ ntfy.el
@@ -4,8 +4,8 @@
 
 ;; Author: Shom Bandopadhaya <shom@bandopadhaya.com>
 ;; Created: 2022-04-30
-;; Modified: 2022-04-30
-;; Version: 0.1.1
+;; Modified: 2022-05-04
+;; Version: 0.1.2
 ;; Keywords: ntfy notification push-notification pub-sub
 ;; Package-Requires: ((emacs "27.2") (curl))
 ;; SPDX-License-Identifier: MIT
@@ -33,58 +33,49 @@
   :type 'string)
 
 (defcustom ntfy-tags nil
-  "Set the emoji that'll appear before the header message.  Use comma separated string, see https://ntfy.sh/docs/publish/#tags-emojis for details."
+  "Set the emoji that'll appear before the header message.
+Use comma separated string, see https://ntfy.sh/docs/publish/#tags-emojis for details."
   :group 'ntfy
   :type 'string)
-
-(defvar ntfy--message nil
-  "Message string that will be published.")
-
-(defvar ntfy--link nil
-  "URL that will be published.")
 
 (defun ntfy-send-message (message)
   "Send ad-hoc MESSAGE from mini-buffer as notification."
   (interactive "sEnter message:")
-  (setq ntfy--message message)
-  (ntfy--publish-message))
+  (ntfy--publish-message message))
 
 (defun ntfy-send-message-with-header (header message)
   "Send ad-hoc MESSAGE from mini-buffer with custom HEADER as notification."
   (interactive "sEnter header: \nsEnter message: ")
   (setq ntfy-header header)
-  (setq ntfy--message message)
-  (ntfy--publish-message))
+  (ntfy--publish-message message header))
 
 (defun ntfy-send-message-with-header-and-tags (tags header message)
-  "Send ad-hoc MESSAGE from mini-buffer with custom HEADER and TAGS as notification."
+  "Send ad-hoc MESSAGE from mini-buffer.
+Custom HEADER and TAGS are set for the notification."
   (interactive "sEnter tags (emoji codes, comma separated no spaces): \nsEnter header: \nsEnter message: ")
-  (setq ntfy-tags tags)
-  (setq ntfy-header header)
-  (setq ntfy--message message)
-  (ntfy--publish-message))
+  (ntfy--publish-message message header tags))
 
-(defun ntfy--publish-message ()
-"Publish message to server with curl."
-(start-process "nfty.el" nil "curl"
-		 "-H" (format "Title: %s" ntfy-header)
-		 "-H" (format "Tags: %s" ntfy-tags)
-		 "-d" (format "%s" ntfy--message)
+(defun ntfy--publish-message (message &optional header tags)
+  "Publish message to server with curl with MESSAGE.
+Configured HEADER and TAGS are used unless specified."
+  (start-process "nfty.el" nil "curl"
+		 "-H" (format "Title: %s" (or header ntfy-header))
+		 "-H" (format "Tags: %s" (or tags ntfy-tags))
+		 "-d" (format "%s" message)
 		 (format "%s/%s" ntfy-server ntfy-topic)))
 
 (defun ntfy-send-url (url)
   "Send URL from mini-buffer."
   (interactive "sEnter URL: \n")
-  (setq ntfy--link url)
-  (ntfy--publish-url))
+  (ntfy--publish-url url))
 
-(defun ntfy--publish-url ()
-"Publish url to server with curl."
+(defun ntfy--publish-url (url)
+"Publish URL to server with curl."
 (start-process "nfty.el" nil "curl"
 		 "-H" (format "Title: %s" "Emacs shared a URL")
 		 "-H" (format "Tags: %s" "link")
-		 "-H" (format "Actions: view, %s, %s" ntfy--link ntfy--link)
-		 "-d" (format "%s" ntfy--link)
+		 "-H" (format "Actions: view, %s, %s" url url)
+		 "-d" (format "%s" url)
 		 (format "%s/%s" ntfy-server ntfy-topic)))
 
 (provide 'ntfy)
